bfd74c8556aaabb1e404e8dc3bea5cc624684dd5
Shih Chiang Work Linux <shih91at@gmail.com>
2017-09-26 16:49:24 +0800

    removed space after F8 for shuhao

--- src/NeuroModulation/command.cpp
+++ src/NeuroModulation/command.cpp
@@ -7,7 +7,6 @@ Command::Command(){
 QByteArray Command::constructCommand(){
     QByteArray outgoingCommand;
     outgoingCommand.append((char) 0xF8);
-    outgoingCommand.append(" ");
     outgoingCommand.append("STIM ");
     outgoingCommand.append(channel + " ");
     outgoingCommand.append(phaseType + " ");
